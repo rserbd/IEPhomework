1a9667108276be385a6214d9825348e5541a08a8
rserbd <r.serbannn@gmail.com>
2022-11-24 23:15:11 +0200

    setup

---
+++ build/CMakeFiles/mealsProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mealsProject.dir/main.cpp.o"
+  "mealsProject"
+  "mealsProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mealsProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/radu/IEP/Meal example
+# Build directory: /home/radu/IEP/Meal example/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
